eb568f698b7a624c3a163684163242320ecfde06
Varun Herlekar <varunherlekar22@gmail.com>
2022-02-20 18:08:36 +0530

    Created Internal Pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import SinglePost from "./components/SinglePost";
+import Post from "./components/Post";
+import Project from "./components/Project";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} exact />
+        <Route path='/about' element={<About />} />
+        <Route path='/post/:slug' element={<SinglePost />} />
+        <Route path='/post' element={<Post />} />
+        <Route path='/project' element={<Project />}/>
+
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
