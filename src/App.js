f1bf604fc9a76478a989e2e59a99601bc852c6c1
Varun Herlekar <varunherlekar22@gmail.com>
2022-02-20 20:21:58 +0530

    Designed Homepage

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
 import Home from "./components/Home";
 import About from "./components/About";
 import SinglePost from "./components/SinglePost";
 import Post from "./components/Post";
 import Project from "./components/Project";
+import NavBar from "./components/NavBar";
 
 function App() {
   return(
     <BrowserRouter>
+    <NavBar/>
       <Routes>
         <Route path='/' element={<Home />} exact />
         <Route path='/about' element={<About />} />
         <Route path='/post/:slug' element={<SinglePost />} />
         <Route path='/post' element={<Post />} />
         <Route path='/project' element={<Project />}/>
-
+        
       </Routes>
     </BrowserRouter>
   )
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,15 @@
 import React from "react";
+import image from "../home-bg.jpg";
 
 export default function Home(){
-    return <h1>Home Page</h1>
+    return(
+        <main>
+            <img src={image} className="absolute object-cover w-full h-full flex-gro" />
+            <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+                <h1 className="text-6xl text-white font-bold cursive leading-none lg:leading-snug">
+                    Bonjour! Welcome to my Blog.
+                </h1>
+            </section>
+        </main>
+    )
 }
